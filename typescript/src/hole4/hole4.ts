7f150cc5b4a4c0c62b1590a3eb2e5c3b44632dbe
Nick Ebbitt <nick.ebbitt@autotrader.co.uk>
2020-04-21 09:17:41 +0100

    Add TypeScript hole4

--- typescript/src/hole4/hole4.ts
+++ typescript/src/hole4/hole4.ts
@@ -6,6 +6,13 @@ export class Money {
         this.value = value;
         this.currency = currency;
     }
+
+    plus(other: Money): Money {
+        if (!(other.currency === this.currency)) {
+            throw new Incalculable();
+        }
+        return new Money(this.value + other.value, other.currency);
+    }
 }
 
 export class Incalculable extends Error {
@@ -26,13 +33,7 @@ export class TakeHomeCalculator {
         let total: Money = first;
 
         monies.forEach((next: Money) => {
-            if (!(next.currency === total.currency)) {
-                throw new Incalculable();
-            }
-        });
-
-        monies.forEach((next: Money) => {
-            total = new Money(total.value + next.value, next.currency);
+            total = total.plus(next);
         });
 
         const amount: number = total.value * (this.percent / 100);
@@ -42,7 +43,7 @@ export class TakeHomeCalculator {
         if (total.currency !== tax.currency) {
             throw new Incalculable();
         }
-        
+
         return new Money(total.value - tax.value, first.currency);
     }
 }
--- typescript/test/hole1/hole1.test.ts
+++ typescript/test/hole1/hole1.test.ts
@@ -4,7 +4,7 @@ import { TakeHomeCalculator, Pair, Incalculable } from '../../src/hole1/hole1';
 describe('Hole 1: Take Home Calculator should', () => {
   it('calculate tax', async () => {
     const first = new TakeHomeCalculator(10).netAmount(new Pair<number, string>(40, "GBP"), new Pair<number, string>(50, "GBP"), new Pair<number, string>(60, "GBP")).first;
-    should(first).equal(135);
+    first.should.equal(135);
   });
 
   it('fail to sum different currencies', () => {
