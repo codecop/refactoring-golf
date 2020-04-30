54fd8b2a8bb39db379f0713a96e016a00d970a24
Nick Ebbitt <nick.ebbitt@autotrader.co.uk>
2020-04-30 12:28:10 +0100

    kotlin: tidy-up

--- kotlin/hole6/src/main/kotlin/TakeHomeCalculator.kt
+++ kotlin/hole6/src/main/kotlin/TakeHomeCalculator.kt
@@ -25,7 +25,7 @@ internal data class Money(val value: Int, val currency: String) {
     }
 
     operator fun minus(other: Money): Money {
-        if (currency != other.currency) {
+        if (other.currency != currency) {
             throw Incalculable()
         }
         return Money(value - other.value, currency)
--- kotlin/hole7/src/main/kotlin/TakeHomeCalculator.kt
+++ kotlin/hole7/src/main/kotlin/TakeHomeCalculator.kt
@@ -5,9 +5,7 @@ class Incalculable : Throwable()
 internal class TakeHomeCalculator(private val taxRate: TaxRate) {
     fun netAmount(first: Money, vararg rest: Money): Money {
         val total = rest.fold(first) { acc, money -> acc + money }
-
         val tax = taxRate.apply(total)
-
         return total - tax
     }
 }
@@ -21,7 +19,7 @@ internal data class Money(val value: Int, val currency: String) {
     }
 
     operator fun minus(other: Money): Money {
-        if (currency != other.currency) {
+        if (other.currency != currency) {
             throw Incalculable()
         }
         return Money(value - other.value, currency)
